ca85548d8b0768046a1f6c8bcc7f30af5ac14f58
SafdarAliGit <safdar211@gmail.com>
2025-04-09 21:50:30 +0500

    updated

--- emadi/emadi/report/weaving_contract_status_report/weaving_contract_status_report.py
+++ emadi/emadi/report/weaving_contract_status_report/weaving_contract_status_report.py
@@ -60,7 +60,7 @@ def get_data(filters):
     LEFT JOIN ( 
         SELECT weaving_contract, SUM(IFNULL(yarn_qty, 0)) AS consumed 
         FROM `tabBOM Items Dn` 
-        GROUP BY weaving_contract
+        GROUP BY weaving_contract  -- Corrected group by clause
     ) bom_item_dn ON bom_item_dn.weaving_contract = wc.name
     LEFT JOIN ( 
         SELECT sed.weaving_contract, SUM(IFNULL(sed.qty, 0)) AS qty 
@@ -72,7 +72,7 @@ def get_data(filters):
     LEFT JOIN ( 
         SELECT custom_weaving_contract, SUM(IFNULL(qty, 0)) AS qty 
         FROM `tabDelivery Note Item` 
-        GROUP BY custom_weaving_contract
+        GROUP BY custom_weaving_contract  -- Corrected group by clause
     ) dni ON dni.custom_weaving_contract = wc.name
     WHERE 
     wc.docstatus = 1
@@ -82,7 +82,7 @@ def get_data(filters):
 
     data = frappe.db.sql(query, filters, as_dict=True)
     # TO REMOVE DUPLICATES
-    keys_to_check = ['contract_name', 'weaver', 'construction', 'fabric','balance_fabric', 'received', 'consumed', 'balance_yarn']
+    keys_to_check = ['contract_name', 'weaver', 'construction']
     seen_values = []
 
     for entry in data:
