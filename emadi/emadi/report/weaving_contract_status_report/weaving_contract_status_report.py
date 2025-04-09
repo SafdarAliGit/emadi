b45879704404838dafe0c879738c72002c6dff43
SafdarAliGit <safdar211@gmail.com>
2025-04-09 21:38:51 +0500

    updated

--- emadi/emadi/report/weaving_contract_status_report/weaving_contract_status_report.py
+++ emadi/emadi/report/weaving_contract_status_report/weaving_contract_status_report.py
@@ -14,11 +14,11 @@ def get_columns():
         {"label": "Construction", "fieldname": "construction", "fieldtype": "Link", "options": "Item", "width": 120},
         {"label": "Fabric Qty", "fieldname": "fabric", "fieldtype": "Data", "width": 100},
         {"label": "Yarn Count", "fieldname": "yarn_count", "fieldtype": "Data", "width": 100},
-        {"label": "Consumption Per LbS", "fieldname": "consumption", "fieldtype": "Float", "width": 140},
-        {"label": "Yarn Required", "fieldname": "required", "fieldtype": "Float", "width": 120},
+        {"label": "Consumption Per LbS", "fieldname": "consumption", "fieldtype": "Data", "width": 140},
+        {"label": "Yarn Required", "fieldname": "required", "fieldtype": "Data", "width": 120},
         {"label": "Yarn Received", "fieldname": "received", "fieldtype": "Data", "width": 120},
-        {"label": "Yarn Consumed", "fieldname": "consumed", "fieldtype": "Float", "width": 120},
-        {"label": "Balance Yarn", "fieldname": "balance_yarn", "fieldtype": "Float", "width": 120},
+        {"label": "Yarn Consumed", "fieldname": "consumed", "fieldtype": "Data", "width": 120},
+        {"label": "Balance Yarn", "fieldname": "balance_yarn", "fieldtype": "Data", "width": 120},
         {"label": "Balance Fabric", "fieldname": "balance_fabric", "fieldtype": "Data", "width": 120}
     ]
 	return columns
@@ -60,7 +60,7 @@ def get_data(filters):
     LEFT JOIN ( 
         SELECT weaving_contract, SUM(IFNULL(yarn_qty, 0)) AS consumed 
         FROM `tabBOM Items Dn` 
-        GROUP BY weaving_contract  -- Corrected group by clause
+        GROUP BY weaving_contract
     ) bom_item_dn ON bom_item_dn.weaving_contract = wc.name
     LEFT JOIN ( 
         SELECT sed.weaving_contract, SUM(IFNULL(sed.qty, 0)) AS qty 
@@ -72,7 +72,7 @@ def get_data(filters):
     LEFT JOIN ( 
         SELECT custom_weaving_contract, SUM(IFNULL(qty, 0)) AS qty 
         FROM `tabDelivery Note Item` 
-        GROUP BY custom_weaving_contract  -- Corrected group by clause
+        GROUP BY custom_weaving_contract
     ) dni ON dni.custom_weaving_contract = wc.name
     WHERE 
     wc.docstatus = 1
@@ -82,7 +82,7 @@ def get_data(filters):
 
     data = frappe.db.sql(query, filters, as_dict=True)
     # TO REMOVE DUPLICATES
-    keys_to_check = ['contract_name', 'weaver', 'construction', 'fabric','balance_fabric', 'received', 'consumed', 'balance_yarn','required', 'yarn_count', 'consumption']
+    keys_to_check = ['contract_name', 'weaver', 'construction', 'fabric','balance_fabric', 'received', 'consumed', 'balance_yarn']
     seen_values = []
 
     for entry in data:
