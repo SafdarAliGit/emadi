5f2744005e5e28515661ff4c109b70c4d387520b
SafdarAliGit <safdar211@gmail.com>
2025-05-17 17:59:53 +0500

    updated

--- emadi/emadi/report/material_flow/material_flow.py
+++ emadi/emadi/report/material_flow/material_flow.py
@@ -122,7 +122,7 @@ def execute(filters=None):
         SELECT
             sp.name AS gate_pass,
             spi.length as bags,
-            spi.lbs
+            ROUND(spi.lbs, 5) AS lbs
         FROM
             `tabSizing Program` AS sp
         LEFT JOIN
@@ -190,7 +190,7 @@ def execute(filters=None):
 
     waste_percentage_bags = float(total_production_length) * (float(p) / 100)
     remaining_bags = total_production_length - waste_percentage_bags
-    yarn_balance_data = [{"posting_date": "<b>Yarn Warp Balance</b>", "gate_pass": "", "yarn_item": "Waste %: " + str(p) + "%", "brand": "Waste: " + str(waste_percentage_bags), "bags": "", "lbs":str(round(remaining_bags,2)-total_warp if total_warp else 0) , "purpose": "Remaining: " + str(round(remaining_bags,2)), "yarn_count": ""}]
+    yarn_balance_data = [{"posting_date": "<b>Yarn Warp Balance</b>", "gate_pass": "", "yarn_item": "Waste %: " + str(p) + "%", "brand": "Waste: " + str(round(waste_percentage_bags,2)), "bags": "", "lbs":str(round(remaining_bags,2)-total_warp if total_warp else 0) , "purpose": "Remaining: " + str(round(remaining_bags,2)), "yarn_count": ""}]
     data.extend(yarn_balance_data)
     
     return columns, data
