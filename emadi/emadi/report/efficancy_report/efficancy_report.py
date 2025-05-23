8b6741b95aa0b5b4e792f8526b34df42aa1635e2
SafdarAliGit <safdar211@gmail.com>
2025-05-23 17:12:44 +0500

    updatged

--- emadi/emadi/report/efficancy_report/efficancy_report.py
+++ emadi/emadi/report/efficancy_report/efficancy_report.py
@@ -23,23 +23,23 @@ def get_columns():
         # Shift A columns
         {"label": "Shift A Loom", "fieldname": "a_loom", "fieldtype": "Data", "width": 120},
         {"label": "Shift A Sizing Name", "fieldname": "a_sizing_name", "fieldtype": "Data", "width": 140},
-        {"label": "Shift A RPM", "fieldname": "a_rpm", "fieldtype": "Float", "width": 80},
-        {"label": "Shift A Unit per RPM", "fieldname": "a_unit_per_rpm", "fieldtype": "Float", "width": 110},
-        {"label": "Shift A Efficiency", "fieldname": "a_effeciency", "fieldtype": "Float", "width": 100},
+        {"label": "Shift A RPM", "fieldname": "a_rpm", "fieldtype": "Data", "width": 80},
+        {"label": "Shift A Unit per RPM", "fieldname": "a_unit_per_rpm", "fieldtype": "Data", "width": 110},
+        {"label": "Shift A Efficiency", "fieldname": "a_effeciency", "fieldtype": "Data", "width": 100},
 
         # Shift B columns
         {"label": "Shift B Loom", "fieldname": "b_loom", "fieldtype": "Data", "width": 120},
         {"label": "Shift B Sizing Name", "fieldname": "b_sizing_name", "fieldtype": "Data", "width": 140},
-        {"label": "Shift B RPM", "fieldname": "b_rpm", "fieldtype": "Float", "width": 80},
-        {"label": "Shift B Unit per RPM", "fieldname": "b_unit_per_rpm", "fieldtype": "Float", "width": 110},
-        {"label": "Shift B Efficiency", "fieldname": "b_effeciency", "fieldtype": "Float", "width": 100},
+        {"label": "Shift B RPM", "fieldname": "b_rpm", "fieldtype": "Data", "width": 80},
+        {"label": "Shift B Unit per RPM", "fieldname": "b_unit_per_rpm", "fieldtype": "Data", "width": 110},
+        {"label": "Shift B Efficiency", "fieldname": "b_effeciency", "fieldtype": "Data", "width": 100},
 
         # Shift C columns
         {"label": "Shift C Loom", "fieldname": "c_loom", "fieldtype": "Data", "width": 120},
         {"label": "Shift C Sizing Name", "fieldname": "c_sizing_name", "fieldtype": "Data", "width": 140},
-        {"label": "Shift C RPM", "fieldname": "c_rpm", "fieldtype": "Float", "width": 80},
-        {"label": "Shift C Unit per RPM", "fieldname": "c_unit_per_rpm", "fieldtype": "Float", "width": 110},
-        {"label": "Shift C Efficiency", "fieldname": "c_effeciency", "fieldtype": "Float", "width": 100},
+        {"label": "Shift C RPM", "fieldname": "c_rpm", "fieldtype": "Data", "width": 80},
+        {"label": "Shift C Unit per RPM", "fieldname": "c_unit_per_rpm", "fieldtype": "Data", "width": 110},
+        {"label": "Shift C Efficiency", "fieldname": "c_effeciency", "fieldtype": "Data", "width": 100},
     ]
     return columns
 
@@ -50,7 +50,7 @@ def get_merged_looms_data(start_date, end_date):
         JOIN `tabLoom Production` m ON d.parent = m.name
         WHERE m.shift = 'Shift-A'
           AND m.date >= %(start_date)s AND m.date <= %(end_date)s
-        ORDER BY d.parent, d.name
+        ORDER BY d.loom
     """, {"start_date": start_date, "end_date": end_date}, as_dict=True)
 
     shift_b = frappe.db.sql("""
@@ -59,7 +59,7 @@ def get_merged_looms_data(start_date, end_date):
         JOIN `tabLoom Production` m ON d.parent = m.name
         WHERE m.shift = 'Shift-B'
           AND m.date >= %(start_date)s AND m.date <= %(end_date)s
-        ORDER BY d.parent, d.name
+        ORDER BY d.loom
     """, {"start_date": start_date, "end_date": end_date}, as_dict=True)
 
     shift_c = frappe.db.sql("""
@@ -68,13 +68,37 @@ def get_merged_looms_data(start_date, end_date):
         JOIN `tabLoom Production` m ON d.parent = m.name
         WHERE m.shift = 'Shift-C'
           AND m.date >= %(start_date)s AND m.date <= %(end_date)s
-        ORDER BY d.parent, d.name
+        ORDER BY d.loom
     """, {"start_date": start_date, "end_date": end_date}, as_dict=True)
 
     max_len = max(len(shift_a), len(shift_b), len(shift_c))
 
     merged_rows = []
 
+    # Add the header row
+    merged_rows.append({
+        'parent': '',
+
+        'a_loom': '<b style="color: green;text-align: center;">----</b>',
+        'a_sizing_name': '<b style="color: green;text-align: center;">----</b>',
+        'a_rpm': '<b style="color: green;text-align: center;">Shift - A</b>',
+        'a_unit_per_rpm': '<b style="color: green;text-align: center;">----</b>',
+        'a_effeciency': '<b style="color: green;text-align: center;">----</b>',
+
+        'b_loom': '<b style="color: orange;text-align: center;">----</b>',
+        'b_sizing_name': '<b style="color: orange;text-align: center;">----</b>',
+        'b_rpm': '<b style="color: orange;text-align: center;">Shift - B</b>',
+        'b_unit_per_rpm': '<b style="color: orange;text-align: center;">----</b>',
+        'b_effeciency': '<b style="color: orange;text-align: center;">----</b>',
+
+        'c_loom': '<b style="color: blue;text-align: center;">----</b>',
+        'c_sizing_name': '<b style="color: blue;text-align: center;">----</b>',
+        'c_rpm': '<b style="color: blue;text-align: center;">Shift - C</b>',
+        'c_unit_per_rpm': '<b style="color: blue;text-align: center;">----</b>',
+        'c_effeciency': '<b style="color: blue;text-align: center;">----</b>',
+    })
+
+    # Add actual data rows
     for i in range(max_len):
         row = {
             'parent': (shift_a[i]['parent'] if i < len(shift_a) else
