b1d28fb0de29e0339a1c3c781f1891db4960e098
SafdarAliGit <safdar211@gmail.com>
2025-05-29 17:48:29 +0500

    updated

--- emadi/emadi/report/material_flow/material_flow.py
+++ emadi/emadi/report/material_flow/material_flow.py
@@ -208,7 +208,7 @@ def execute(filters=None):
 
     waste_percentage_bags = float(total_production_length) * (float(p) / 100)
     remaining_bags = total_production_length - waste_percentage_bags
-    yarn_balance_data = [{"posting_date": "<b>Yarn Warp Balance(Length)</b>", "gate_pass": "", "yarn_item": "Waste %: " + str(p) + "%", "brand": "Waste: " + str(round(waste_percentage_bags,2)), "bags":str(round(remaining_bags,2)-total_warp if total_warp else 0) , "lbs":"", "purpose": "Remaining: " + str(round(remaining_bags,2)), "yarn_count": ""}]
+    yarn_balance_data = [{"posting_date": "<b>Yarn Warp Balance(Length)</b>", "gate_pass": "", "yarn_item": "Waste %: " + str(p) + "%", "brand": "Waste: " + str(round(waste_percentage_bags,2)), "bags":str(round(remaining_bags,2)-total_warp if total_warp else 0) , "lbs":"", "purpose": "Remaining: " + str(round((remaining_bags * ratio if ratio else 1),2)), "yarn_count": ""}]
     data.extend(yarn_balance_data)
     # Delivery Detail
     delivery_fabric_qty = frappe.db.sql(f"""
