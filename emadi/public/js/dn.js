91ef1cd8dc7cf8386f47b7a11d588e0a9c9c048d
SafdarAliGit <safdar211@gmail.com>
2025-10-20 17:15:47 +0500

    updated

--- emadi/public/js/dn.js
+++ emadi/public/js/dn.js
@@ -14,5 +14,25 @@ frappe.ui.form.on('Delivery Note', {
         }
         }
         
+    },
+    fabric_item: function(frm) {
+        // When the fabric_item is changed, fetch the item doc
+        if (frm.doc.fabric_item) {
+            frappe.db.get_doc("Item", frm.doc.fabric_item)
+                .then(item_doc => {
+                    // Check the custom field and set custom_non_conversion accordingly
+                    if (item_doc.custom_conversion === 1) {
+                        frm.set_value("custom_non_conversion", 1);
+                    } else {
+                        frm.set_value("custom_non_conversion", 0);
+                    }
+                });
+        } else {
+            // No item selected, default
+            frm.set_value("custom_non_conversion", 0);
+        }
     }
 });
+
+
+                
