51c0d84901677a930a493f32824d5b9326f2d13c
SafdarAliGit <safdar211@gmail.com>
2025-12-01 22:21:51 +0500

    updatd

---
+++ emadi/emadi/doctype/fabric_production_out_side/fabric_production_out_side.js
@@ -0,0 +1,157 @@
+// Copyright (c) 2025, Safdar Ali and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Fabric Production Out Side', {
+	refresh: function(frm) {
+        frm.set_query('target_warehouse', function() {
+            return {
+                "filters": [
+					["is_group", "=", 0 ]
+				]
+            };
+        });
+        
+		frm.set_query('yarn_count','fabric_production_out_side_item', function() {
+			return {
+				"filters": [
+					["item_group", "in", ["Yarn","Beam"]]
+				]
+			}
+		});
+        frm.set_query('set_no','fabric_production_out_side_item', function() {
+            return {
+                "filters": [
+                    ["item", "=", frm.doc.yarn_count]
+                ]
+            }
+        })
+        frm.set_query('set_no', 'fabric_production_out_side_item', function(doc, cdt, cdn) {
+			let child = locals[cdt][cdn];
+			return {
+				filters: {
+					item: child.yarn_count
+				}
+			};
+		});
+	},
+    weaving_charges: function(frm) {
+        var weaving_charges = frm.doc.weaving_charges;
+        var qty = frm.doc.qty || 0;
+        var amount = weaving_charges * qty;
+        frm.set_value("amount", amount);
+    },
+	quality: function(frm) {
+        if (frm.doc.quality && frm.doc.qty) {
+            frappe.call({
+                method: "emadi.emadi.events.fetch_fabric_construction_items.fetch_fabric_construction_items",
+                args: {
+                    quality: frm.doc.quality,
+                    qty: frm.doc.qty
+                },
+                callback: function(r) {
+                    if (r.message) {
+                        frm.clear_table("fabric_production_out_side_item");
+                        $.each(r.message, function(_, item) {
+                            let row = frm.add_child("fabric_production_out_side_item");
+                            row.for = item.for;
+                            row.yarn_count = item.yarn_count;
+                            row.consumption = item.consumption;
+                            row.yarn_qty = item.yarn_qty;
+                        });
+                        frm.refresh_field("fabric_production_out_side_item");
+                    }
+                }
+            });
+        }
+    },
+    qty: function(frm) {
+        // Recalculate when qty changes
+        frm.trigger("quality");
+    }
+});
+
+frappe.ui.form.on('Fabric Production Out Side Item', {
+    
+    warehouse(frm, cdt, cdn) {
+        const row = locals[cdt][cdn];
+        // Only proceed if essential fields are present
+       
+        frm.call({
+            method: 'emadi.emadi.events.fetch_current_stock.fetch_current_stock',
+            args: {
+                item_code: row.yarn_count,
+                warehouse: row.warehouse,
+                posting_date: frm.doc.posting_date
+                
+            },
+            callback: ({ message }) => {
+                const available = message?.current_stock || 0;
+                const rate = message?.rate || 0;
+                const yarnQty = row.yarn_qty || 0;
+            
+
+                frappe.model.set_value(cdt, cdn, 'available_qty', available);
+                frappe.model.set_value(cdt, cdn, 'valuation_rate', rate);
+                frappe.model.set_value(cdt, cdn, 'rate_per_meter', row.consumption * rate);
+                frappe.model.set_value(cdt, cdn, 'amount', rate * yarnQty);
+
+                recalculate_rate(frm);
+            },
+            error: err => {
+                frappe.msgprint({
+                    title: __('Stock Fetch Error'),
+                    message: __('Could not load stock data for row {0}', [row.idx]),
+                    indicator: 'red'
+                });
+            }
+        });
+    },
+    yarn_count: function(frm) {
+        // Recalculate when qty changes
+        row.trigger("warehouse");
+    }
+})
+
+
+// Shared logic to total up amounts and calculate rate
+function recalculate_rate(frm) {
+    const items = frm.doc.fabric_production_out_side_item || [];
+    let warp_rate = 0;
+    let weft_rate = 0;
+   items.forEach(element => {
+    if (element['for'] === "Warp") {
+        warp_rate += flt(element.rate_per_meter);
+    } else if (element['for'] === "Weft") {
+        weft_rate += flt(element.rate_per_meter);
+    }
+    
+   });
+
+    let avg_rate = 0;
+ 
+      avg_rate = warp_rate + weft_rate;
+      frm.set_value('warp_rate', warp_rate);
+      frm.set_value('weft_rate', weft_rate);
+      frm.set_value('finish_rate', avg_rate);
+    
+    
+    if (frm.doc.valuation_type === 0 && avg_rate <= 0) {
+      frappe.throw(__('Rate must be greater than zero'));
+    }
+  }
+frappe.ui.form.on('Fabric Production Other Charges Item', {
+    amount: function(frm, cdt, cdn) {
+
+        let total_charges = 0;
+
+        // Loop through all rows of the child table
+        (frm.doc.fabric_production_other_charges_item || []).forEach(row => {
+            total_charges += row.amount || 0;
+        });
+
+        // Set total in parent doctype
+        frm.set_value('total_charges', total_charges);
+    }
+});
+
+    
---
+++ emadi/emadi/doctype/fabric_production_out_side/fabric_production_out_side.py
@@ -0,0 +1,116 @@
+# Copyright (c) 2025, Safdar Ali and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+from frappe.model.naming import make_autoname
+from emadi.emadi.doctype.utils_functions import get_doctype_by_field
+
+class FabricProductionOutSide(Document):
+	
+	# def on_update(self):
+	# 	for item in self.fabric_production_item:
+	# 		if item.get("for") == "Weft":
+	# 			item.warehouse = "Stores - ET"
+	# 		elif item.get("for") == "Warp":
+	# 			item.warehouse = "Beem Store - ET"
+		
+
+	def on_submit(self):
+		if self.fabric_production_out_side_item:
+			doc = frappe.new_doc("Stock Entry")
+			doc.stock_entry_type = "Manufacture"
+			doc.purpose = "Manufacture"
+			doc.set_posting_time = 1
+			doc.posting_date = self.posting_date
+			doc.posting_time = self.posting_time
+			doc.custom_fabric_production_out_side = self.name
+			target_warehouse = self.target_warehouse
+		
+			# Append source item
+			it = doc.append("items", {})
+			it.t_warehouse = target_warehouse
+			it.item_code = self.fabric_item
+			it.qty = self.qty
+			it.is_finished_item = 1
+			if self.valuation_type == 1:
+				it.allow_zero_valuation_rate = 1
+			else:
+				it.allow_zero_valuation_rate = 0
+				it.set_basic_rate_manually = 1
+				it.basic_rate = self.finish_rate
+				it.basic_amount = self.finish_rate * self.qty
+			
+				
+			# Append target items using a loop
+			for item in self.fabric_production_out_side_item:
+				it = doc.append("items", {})
+				it.s_warehouse = item.warehouse
+				it.item_code = item.yarn_count
+				if item.set_no:
+					it.batch_no = item.set_no
+				if item.beam_length:
+					it.qty = item.beam_length
+				else:
+					it.qty = item.yarn_qty
+				if self.valuation_type == 1:
+					it.allow_zero_valuation_rate = 1
+				else:
+					it.allow_zero_valuation_rate = 0
+					
+
+			try:
+				if self.valuation_type == 0 and self.finish_rate > 0:
+					doc.save()
+				elif self.valuation_type == 1:
+					doc.save()
+				else:
+					frappe.throw("Rate must be greater than zero")
+					
+					# updat stock entry
+					# if self.valuation_type == 0:
+					# 	stock_entry = frappe.get_doc("Stock Entry", doc.name)
+					# 	total_amount = stock_entry.total_outgoing_value or 0.0
+				# 	single_target_row = None
+
+				# 	# pick the first item with a target warehouse set
+				# 	for item in stock_entry.items:
+				# 		if getattr(item, 't_warehouse', None):
+				# 			single_target_row = item
+				# 			break
+
+				# 	if single_target_row and single_target_row.qty:
+				# 		qty = float(single_target_row.qty) or 0.0
+				# 		if qty > 0:
+				# 			new_rate = total_amount / qty
+				# 			single_target_row.basic_rate = new_rate
+				# 			single_target_row.basic_amount = new_rate * qty
+				# 		else:
+				# 			frappe.throw(f"Invalid quantity {single_target_row.qty} for computing rate")
+				# 	# save and submit
+				# 	stock_entry.reload()  # Reload before saving
+				# 	stock_entry.save()
+				# 	frappe.db.commit()  # Commit the rate calculation changes
+		
+				# doc.reload()
+				doc.submit()
+			except Exception as e:
+				frappe.throw("Error submitting Stock Entry: {0}".format(str(e)))	
+
+
+	def on_cancel(self):
+		current_je = get_doctype_by_field('Stock Entry', 'custom_fabric_production_out_side', self.name)
+		if current_je:
+			if current_je.docstatus != 2:  # Ensure the document is in the "Submitted" state
+				current_je.cancel()
+				frappe.db.commit()
+			else:
+				frappe.throw("Document is not in the 'Submitted' state.")
+			if current_je.amended_from:
+				new_name = int(current_je.name.split("-")[-1]) + 1
+			else:
+				new_name = f"{current_je.name}-{1}"
+			make_autoname(new_name, 'Stock Entry')
+		
+        
+        
